9eb611a5dc73c78efe188fc6152ba1f0cf031d2e
mojisilva <mojiasilva@gmail.com>
2022-03-08 19:38:17 -0300

    criação do projeto de gerador de números da mega sena

---
+++ src/main/java/com/numerosmega/NumerosMegaApplication.java
@@ -0,0 +1,25 @@
+package com.numerosmega;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NumerosMegaApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(NumerosMegaApplication.class.getResource("numerosmega-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 130);
+        stage.setTitle("Gerador Mega-Sena");
+        stage.getIcons().add(new Image(NumerosMegaApplication.class.getResourceAsStream("icon.png")));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/numerosmega/NumerosMegaController.java
@@ -0,0 +1,37 @@
+package com.numerosmega;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.util.LinkedHashSet;
+import java.util.Random;
+
+public class NumerosMegaController {
+    Random gerador = new Random(0);
+    LinkedHashSet<Integer> numeros = new LinkedHashSet<>();
+    String formato;
+
+    @FXML
+    private TextField inputTF;
+
+    @FXML
+    private Button fecharBT;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        for (int i = 0; i<6; i++){
+            while(numeros.size() <6 ){
+                numeros.add(gerador.nextInt(1, 60));
+            }
+            formato = String.valueOf(numeros);
+
+            inputTF.setText(formato.replace("[", "").replace(",", " -").replace("]", ""));
+        }
+        numeros.clear();
+        fechar();
+    }
+
+    private void fechar() {
+        fecharBT.setOnAction(ActionEvent -> System.exit(0));
+    }
+}
